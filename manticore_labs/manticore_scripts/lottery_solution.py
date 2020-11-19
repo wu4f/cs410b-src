ac408d233217688537090c18a7a48a8856c7c6f8
wu4f <wuchang@pdx.edu>
2020-11-18 18:27:54 -0800

    new version

--- manticore_labs/manticore_scripts/lottery_solution.py
+++ manticore_labs/manticore_scripts/lottery_solution.py
@@ -1,18 +1,24 @@
+'''
+To solve this level, we'll need to fill in several contract values
+similar to prior levels, including the amount that is needed to play
+the lottery
+'''
 from manticore.ethereum import ABI, ManticoreEVM
-import binascii
 import sys
-import sha3
 
-m = ManticoreEVM()
-m.verbosity(0)
-# We need to get the addresses for our wallet and the CTF contract. These will be strings in hex format
+# Parse arguments
+#   arg1 = from_address = Your wallet address
+#   arg2 = si_level_address = Your CTF level address
+#   arg3 = sol_file = CTF level source code to symbolically execute
 from_address = int(sys.argv[1], 16) if len(sys.argv)>1 else "<your address here>"
 si_level_address = int(sys.argv[2], 16) if len(sys.argv)>2 else "<SI ctf level address>"
 sol_file = sys.argv[3] if len(sys.argv)>3 else "../SI_ctf_levels/Lottery.sol"
 gas = 100000
-# We need to know how much ether is in the contract. This value will be an int and is in wei
+
+# Set the amount of ETH you want to obtain from the contract
 contract_balance = ???
-# We need to give our contract a msg value. This value will be in wei
+
+# Set the amount of ETH we need to send in our transaction (msg.value) to play.
 msg_value = ???
 
 # read in the contract source
@@ -21,38 +27,47 @@ with open(sol_file, "r") as f:
 
 # instantiate manticore's Ethereum Virtual Machine
 m = ManticoreEVM()
+# m.verbosity(0)
 
-# create a virtual user account on the EVM
-user_account = m.create_account(address=from_address, balance=contract_balance+ msg_value)
-print('Created virtual user account...')
+# Create an account for your wallet address on the EVM with funds to
+# both deploy the contract and to play the lottery
+user_account = m.create_account(address=from_address, balance=contract_balance+msg_value)
 
-# create our contract
+# Create the Lottery CTF level contract on the EVM using wallet
 contract_account = m.solidity_create_contract(
-    contract_src, # a string containing the source file
-    contract_name="Lottery", # the contract in the source file that we want to create (a file could have multiple contracts)
-    owner=user_account, # the creator address for this first exploit, we can use our own account to create it (this may not be true for future levels)
-    balance=contract_balance, # the value to send to the contructor (this will actually be deducted from our virtual address)
-    args=(0,0) # in the downloaded contract, these arguments aren't used
-     )
-print('Created contract...')
+    contract_src,
+    contract_name="Lottery",
+    owner=user_account,
+    balance=contract_balance,
+    args=(0,0)
+)
 
-# This will be our data constraint in our transaction
-buff = m.make_symbolic_buffer(???)
-# Now, we constrain the execution with a transaction
-m.transaction(caller=user_account, address=contract_account.address, data=buff, value = msg_value, gas=gas)
+# Make symbolic buffer to hold msg.data and ask Manticore to calculate the "winning" value
+# 4 bytes for the function signature hash and ??? more for a uint256
+sym_args = m.make_symbolic_buffer(???)
 
+# Issue a symbolic transaction to the EVM by setting msg.data to symbolic buffer
+#   as well as msg.value to the amount needed to play
+m.transaction(
+    caller=user_account,
+    address=contract_account.address,
+    data=sym_args,
+    value=msg_value,
+    gas=gas
+)
 
-# for states that are still running (haven't reverted due to our transaction contraint) let's iterate through them and see if any allow for an exploit
+# Symbolically execute program to find an exploit that obtains our funds back.
 for state in m.running_states:
     # this is just some silly manticore stuff
     world = state.platform
-    # stealing back all the ether is a good way of proving that an exploit exists
+    # Check if funds can be retrieved
     if state.can_be_true(world.get_balance(user_account.address) == contract_balance + msg_value):
-        state.constraints.add(world.get_balance(user_account.address) == contract_balance + msg_value)
-        result = state.solve_one(buff)
-        # print out our transaction
-        print("eth.sendTransaction({data:\"0x"+binascii.hexlify(result).decode('utf-8')+"\", from:\""+ hex(from_address) +
-            "\", to:\""+hex(si_level_address)+"\", value:"+str(msg_value)+", gas:"+str(gas)+"})")
-        # this prints the exploit out in a format that you can easily paste into geth to win
-        sys.exit(0) # we only needed one state!
+      # If so, add constraint
+      #   Then concretize symbolic buffer to provide one solution
+      state.constraints.add(world.get_balance(user_account.address) == contract_balance + msg_value)
+      conc_args = state.solve_one(sym_args)
+      # Print out our transaction to send to win
+      print("eth.sendTransaction({data:\"0x"+binascii.hexlify(conc_args).decode('utf-8')+"\", from:\""+ hex(from_address) + "\", to:\""+hex(si_level_address)+"\", value:"+str(msg_value)+", gas:"+str(gas)+"})")
+      print(f'''eth.sendTransaction({{data:"0x{conc_args.hex()}", from:"{hex(from_address)}", to:"{hex(si_level_address)}", value: {value}, gas:{gas}}})''')
+      sys.exit(0)
     print('No state found')
